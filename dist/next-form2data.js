6906c764011ffc5244ca7930b34313f8c73f3fc5
afeiship <1290657123@qq.com>
2019-12-30 10:28:42 +0800

    feat: to pure es5 version

--- dist/next-form2data.js
+++ dist/next-form2data.js
@@ -3,7 +3,7 @@
  * description: FormData to js data.
  * url: https://github.com/afeiship/next-form2data
  * version: 1.0.0
- * date: 2019-12-26 17:33:08
+ * date: 2019-12-30 10:28:21
  * license: MIT
  */
 
@@ -12,10 +12,9 @@
   var nx = global.nx || require('@feizheng/next-js-core2');
 
   nx.form2data = function(inFormData) {
-    var keys = Array.from(inFormData.keys());
     var result = {};
-    keys.forEach(function(key) {
-      result[key] = inFormData.get(key);
+    inFormData.forEach(function(value, key) {
+      result[key] = value;
     });
     return result;
   };
--- src/next-form2data.js
+++ src/next-form2data.js
@@ -3,10 +3,9 @@
   var nx = global.nx || require('@feizheng/next-js-core2');
 
   nx.form2data = function(inFormData) {
-    var keys = Array.from(inFormData.keys());
     var result = {};
-    keys.forEach(function(key) {
-      result[key] = inFormData.get(key);
+    inFormData.forEach(function(value, key) {
+      result[key] = value;
     });
     return result;
   };
